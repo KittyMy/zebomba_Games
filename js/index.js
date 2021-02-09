c9ef57afdd107bf451c664a8c20f4f9e4f6ec9e7
Kitty <58696697+KittyMy@users.noreply.github.com>
2021-02-09 23:24:40 +0500

    second work

--- js/index.js
+++ js/index.js
@@ -1,49 +1,50 @@
 let container = document.querySelector('.container'),
-    go = document.getElementById('go'),
-    woman = document.getElementById('woman_sm');
+    go = document.getElementById('go');
 
-// const arr = [
-//     0, 524, 450, 363, 282, 296, 314, 387, 350, 311, 375, 426, 470, 504,
-//     545, 593, 679, 633, 659, 629, 558, 548, 
-//     626, 698, 797, 888, 984, 1057, 1120, 1134, 1076, 1015,
-//     958, 894, 897, 969, 983, 926, 853, 796, 755, 694, 680,
-//     720, 762, 788, 820, 870, 845, 926, 993, 1040, 1058
-// ];
-const arr = [0, 300, 400];
+const arr = [
+    0, 125, 200, -147, -80, 296, 314, 387, 350, 311, 375, 426, 470, 504,
+    545, 593, 679, 633, 659, 629, 558, 548, 
+    626, 698, 797, 888, 984, 1057, 1120, 1134, 1076, 1015,
+    958, 894, 897, 969, 983, 926, 853, 796, 755, 694, 680,
+    720, 762, 788, 820, 870, 845, 926, 993, 1040, 1058
+];
+
+
     // container.onclick = function(e) {
         // console.log(e.clientX, 'Eto X');
         // console.log(e.clientY, 'Eto Y');
     
-        // console.log(e.clientX);
-        // console.log(e.clientY);
+        // console.log(path.getPointAtLength(e.clientX), 'etoX');
+        // console.log(path.getPointAtLength(e.clientY), 'etoY');
+       
+        // console.log(path.getPointAtLength(e));
         
         // woman.getBoundingClientRect().x = e.clientX;
         // woman.style.bottom = e.clientY;
     // };
 
     function positionCar() {
-        // var scrollY = window.scrollY || window.pageYOffset;
-        // var maxScrollY = document.documentElement.scrollHeight - window.innerHeight;
-
-        var path = document.getElementById("path1");
-        var woman = document.getElementById('woman');
       
+        let path = document.getElementById("path1");
+
         var pathLen = path.getTotalLength();
 
         var dist = arr.shift();
-        console.log(dist);
-        
-        var pos = path.getPointAtLength(dist);
-        console.log(pos);
-        // console.log(pos.x);
-        // console.log(pos.y);
+        // console.log(dist.a, 'x');
+        // console.log(dist.b, 'y');
 
-        woman.setAttribute("transform", "translate(" + pos.x + "," + pos.y + ")");
+        // console.log(path.getPointAtLength(dist.a), 'ha-ha')
+        // console.log(path.getPointAtLength(dist.b).y, 'ho-ho')
+
+        // path.getPointAtLength(dist.a).x = dist.a
+        
+        let pos = path.getPointAtLength(dist);
+        // console.log(pos, 'position');
+
+        let woman = document.getElementById('woman_sm');
+        woman.setAttribute("transform", `translate(${pos.x},${pos.y})`);
 
       }
-      
-    //   window.addEventListener("scroll", positionCar);
-
-    go.addEventListener('click', positionCar);
-      
+    
+      go.addEventListener('click', positionCar);
       positionCar();
